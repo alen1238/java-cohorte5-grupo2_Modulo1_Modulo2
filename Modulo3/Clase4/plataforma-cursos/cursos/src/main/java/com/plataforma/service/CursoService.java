a609ea73be0cd177b1d23832e512195aed0a2035
Alfonso Lara <orozcoenrique159@gmail.com>
2026-02-07 16:00:32 -0500

    servicio agregado parcialmente

---
+++ Modulo3/Clase4/plataforma-cursos/cursos/src/main/java/com/plataforma/service/CursoService.java
@@ -0,0 +1,41 @@
+package com.plataforma.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.logging.log4j.*;
+
+import com.plataforma.exception.CursoLlenoException;
+import com.plataforma.model.Curso;
+
+public class CursoService {
+    private static final Logger logger = LogManager.getLogger(CursoService.class);
+
+    private List<Curso> cursos;
+
+    public CursoService() {
+        logger.info("CursoService initialized");
+        cursos = new ArrayList<>();
+    }
+
+    public void addCurso(Curso curso) {
+        logger.info("Adding curso: " + curso.getNombre());
+        cursos.add(curso);
+    }
+
+    public List<Curso> getCursos() {
+        logger.info("Retrieving list of cursos");
+        return cursos;
+    }
+
+    public Curso buscarCursoPorCodigo(String codigo){
+        for(Curso curso : cursos){
+            if(curso.getCodigo().equals(codigo)){
+                logger.info("Curso found: " + curso.getNombre());
+                return curso; //este return permite salir del método una vez que se encuentra el curso
+            }
+        }
+        logger.warn("Curso not found for codigo: " + codigo);
+        throw new CursoLlenoException("Curso con código " + codigo + " no fue encontrado");
+    }
+}
